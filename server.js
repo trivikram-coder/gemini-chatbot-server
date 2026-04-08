c917649386474773a4d5a896308390c52998e429
vikram-42-alla <allatrivikram@gmail.com>
2026-04-08 21:18:49 +0530

    Refactor server.js: remove body-parser dependency and update server log message

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+
 const dotenv = require("dotenv");
 const routes=require("./Routes/routes")
 
@@ -18,5 +18,5 @@ app.get("/",(req,res)=>{
 connectDb()
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`GEMINI Server running on http://localhost:${PORT}`);
 });
